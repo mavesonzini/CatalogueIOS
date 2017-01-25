64b8da34d8818c99a5cac44408bc811b3f9b4e4c
Veronica Sonzini <mavesonzini@gmail.com>
2017-01-25 11:50:35 +0000

    ItemList.swift and itemListTests.swift added
    ItemList is the Controller for the tableView that will show the list of items

---
+++ Catalogue/Catalogue/ItemListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ItemListViewController.swift
+//  Catalogue
+//
+//  Created by María Verónica Sonzini on 25/01/2017.
+//  Copyright © 2017 María Verónica Sonzini. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+    var tableView: UITableView?
+
+    override func viewDidLoad() {
+        tableView = UITableView()
+    }
+
+}
