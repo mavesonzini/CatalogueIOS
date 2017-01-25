f9b6e4ce2d444c302031b813a39ea5460b209a12
Veronica Sonzini <mavesonzini@gmail.com>
2017-01-25 10:58:23 +0000

    First commit
    Catalogue Project created. ItemTest.swift file created.

---
+++ Catalogue/CatalogueTests/ItemTest.swift
@@ -0,0 +1,27 @@
+//
+//  ItemTest.swift
+//  Catalogue
+//
+//  Created by María Verónica Sonzini on 25/01/2017.
+//  Copyright © 2017 María Verónica Sonzini. All rights reserved.
+//
+
+import XCTest
+
+class ItemTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func  testInit_shouldTakeTitle(){
+        Item(title:"Test title")
+    }
+    
+}
